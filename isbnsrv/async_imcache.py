e78b93da9d0dc66dbc4e820ec3df56671ebb4224
Alexandre Conde <alexandreconde@hotmail.com>
2019-05-16 12:44:51 +0100

    fix 'async_imcache'

--- isbnsrv/async_imcache.py
+++ isbnsrv/async_imcache.py
@@ -17,45 +17,26 @@ class MemoryCache(MutableMapping):
         while len(self.d) > maxlen:  # pragma: no cache
             self.d.popitem()
 
-    async def __iter__(self):
-        return iter(self.d)
-
-    async def __len__(self):
+    async def len(self):
         return len(self.d)
 
-    async def __getitem__(self, k):
-        try:
-            return self.d[k]
-        except KeyError:
-            return None
+    async def has(self, key):
+        return key in self.d
 
-    async def __setitem__(self, k, v):
+    async def delitem(self, k):
+        del self.d[k]
+
+    async def set(self, k, v):
         if k not in self.d and len(self.d) == self.maxlen:
             self.d.popitem()
         if v:
             self.d[k] = v
 
-    async def has(self, key):
-        return key in self.d
-
-    async def __delitem__(self, k):
-        del self.d[k]
-
-    async def __bool__(self):
-        return len(self.d) != 0
-
-    async def __call__(self, k):
-        """Implement function call operator."""
+    async def get(self, k):
         try:
-            return await self.__getitem__(k)
+            return self.d[k]
         except KeyError:
             return None
-
-    async def set(self, k, v):
-        return await self.__setitem__(k, v)
-
-    async def get(self, k):
-        return await self.__getitem__(k)
 
     async def get_key(self, request):
         key = "{method}#{host}#{path}#{postdata}#{ctype}".format(
