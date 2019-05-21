adc004c261cacd8d47bd78654c041f08f77199c7
Alexandre Conde <alexandreconde@hotmail.com>
2019-05-21 09:13:56 +0100

    add 'test_api'

---
+++ tests/test_api.py
@@ -0,0 +1,88 @@
+# import pytest
+from isbnsrv.server import app
+
+# FIXME
+
+
+async def test_api(aiohttp_client):
+    client = await aiohttp_client(app)
+
+    resp = await client.get("/api/v1/isbns/9780140440393/isbn10")
+    assert resp.status == 200
+    text = await resp.text()
+    assert '{"isbn10": "0140440399"}' == text
+
+    resp = await client.get("/api/v1/isbns/9780140440393/isbn13")
+    assert resp.status == 200
+    text = await resp.text()
+    assert '{"isbn13": "9780140440393"}' == text
+
+    resp = await client.get("/api/v1/isbns/9780140440393/mask")
+    assert resp.status == 200
+    text = await resp.text()
+    assert '{"mask": "978-0-14-044039-3"}' == text
+
+    resp = await client.get("/api/v1/isbns/9780140440393/info")
+    assert resp.status == 200
+    text = await resp.text()
+    assert '{"info": "English language"}' == text
+
+    resp = await client.get("/api/v1/isbns/9780140440393/metadata")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "History Of The Peloponnesian War" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393?fields=metadata")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "History Of The Peloponnesian War" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393/metadata/goob")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "Thucydides" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393?fields=metadata,goob")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "History Of The Peloponnesian War" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393/description")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "four hundred years" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393?fields=description")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "four hundred years before the birth of Christ" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393/cover")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "id=RQo1YIvlwRAC" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393?fields=cover")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "id=RQo1YIvlwRAC" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393/editions")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "9780140440393" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393?fields=editions")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "9780140440393" in text
+
+    resp = await client.get("/api/v1/isbns/9780140440393")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "9780140440393" in text
+
+    resp = await client.get("/api/v1/isbns/978014044039")
+    assert resp.status == 404
+    text = await resp.text()
+    assert "ERROR" in text
