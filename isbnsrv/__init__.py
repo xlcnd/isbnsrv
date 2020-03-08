bb31135c7557a058db9364d072edd6c2df5c388e
Alexandre Conde <alexandreconde@hotmail.com>
2020-03-08 09:28:41 +0000

    fix version

--- isbnsrv/__init__.py
+++ isbnsrv/__init__.py
@@ -1,7 +1,7 @@
 import concurrent.futures
 
 __api__ = "1"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 SERVER = {"Server": f"isbnsrv/{__version__}"}
 executor = concurrent.futures.ThreadPoolExecutor()  # max_workers=(5 x #cores)
