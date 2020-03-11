74f267a472489f9c4e13038616f2ad8d12f9e285
Alexandre Conde <alexandreconde@hotmail.com>
2020-03-11 18:05:10 +0000

    [WIP] add GRAPHQL API (asyncio)

--- isbnsrv/graphql_api.py
+++ isbnsrv/graphql_api.py
@@ -3,11 +3,8 @@
 import asyncio
 import logging
 
-from json import dumps
-
 # from aiohttp import web
 from graphene import Field, List, ObjectType, String, Schema
-from graphql.execution.executors.asyncio import AsyncioExecutor
 
 from . import executor
 from .resources import (
@@ -132,87 +129,3 @@ class Query(ObjectType):
 
 
 schema = Schema(query=Query, auto_camelcase=True)
-
-
-def run():
-
-    # result = schema.execute(
-    #     """
-    #      query FullIsbn($isbn: String!) {
-    #        fullIsbn(isbn: $isbn) {
-    #          isbn13
-    #          ean13
-    #          doi
-    #          isbn10
-    #          info
-    #        }
-    #      }
-    #    """,
-    #     variables={"isbn": "9780140440393"},
-    # executor=AsyncioExecutor(),)
-
-    q = """
-          query Search {
-            search(searchTerms: "reveries of a solitary walker") {
-                isbn13
-                title
-                authors { name }
-                publisher
-                year
-                language
-            }
-          }
-        """
-
-    # q = """
-    #       query FullIsbn {
-    #         fullIsbn(isbn: "9780140440393") {
-    #           isbn13
-    #           ean13
-    #           doi
-    #           isbn10
-    #           info
-    #         }
-    #       }
-    #     """
-
-    # q = """
-    #       query Providers {
-    #         metadataDublinCoreProviders {
-    #           name
-    #         }
-    #       }
-    #     """
-
-    # q = """
-    #       query MetadataDublinCore {
-    #         metadataDublinCore(isbn: "9780192821911") {
-    #           isbn13
-    #           title
-    #           authors { name }
-    #           publisher
-    #           year
-    #           language
-    #         }
-    #      }
-    #    """
-
-    # q = """
-    #       query MetadataExtra {
-    #         metadataExtra(isbn: "9780192821911") {
-    #           description
-    #           covers { size, url }
-    #           identifiers { isbn13, doi, owi, ddc, fast { numericId, classText } }
-    #           editions
-    #         }
-    #       }
-    #     """
-
-    result = schema.execute(q, executor=AsyncioExecutor())
-
-    # assert not result.errors
-    # with open('result.json', 'w', encoding='utf8') as json_file:
-    #    json.dump(result.data, json_file, ensure_ascii=False)
-    print(dumps(result.data, ensure_ascii=False).encode("utf8").decode())
-
-    # print(dumps(schema.introspect()))
